cb3ae974f15f3f9b1327357254d401d7cb6df178
jose hernandez <josegho2013@gmail.com>
2022-02-08 22:24:55 -0300

    .

--- appointment-manager/src/components/Appointment_manager.jsx
+++ appointment-manager/src/components/Appointment_manager.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "../css/styles.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Table,
@@ -15,10 +17,12 @@ import {
 } from "reactstrap";
 
 const Appointment_manager = () => {
-  const [modalInsertar, setModalInsertar] = useState(false);
   const [citas, setCitas] = useState([]);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
 
   const [input, setInput] = useState({
+    id: citas?.length + 1,
     nombre_de_mascota: "",
     propietario: "",
     telefono: "",
@@ -39,48 +43,131 @@ const Appointment_manager = () => {
       [e.target.name]: e.target.value,
     });
   };
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     let lista = citas;
     lista.push(input);
     setCitas(lista);
+    setModalInsertar(false);
+
+    setInput({
+      id: citas?.length + 1,
+      nombre_de_mascota: "",
+      propietario: "",
+      telefono: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
   };
+
+  const mostrarModalEditar = (dato) => {
+    setInput(dato);
+    setModalEditar(true);
+  };
+
+  const cerrarModalEditar = () => {
+    setModalEditar(false);
+  };
+  const editar = (dato) => {
+    var contador = 0;
+    var arreglo = citas;
+
+    arreglo.map((registro) => {
+      if (dato.id === registro.id) {
+        arreglo[contador].nombre_de_mascota = dato.nombre_de_mascota;
+        arreglo[contador].propietario = dato.propietario;
+        arreglo[contador].fecha = dato.fecha;
+        arreglo[contador].telefono = dato.telefono;
+        arreglo[contador].hora = dato.hora;
+        arreglo[contador].sintomas = dato.sintomas;
+      }
+      contador++;
+    });
+    setInput(arreglo);
+    setModalEditar(false);
+  };
+
   return (
     <div>
       <h1>Appointment Manager</h1>
+      <Container>
+        <Button color="success" onClick={() => mostrarModalInsertar()}>
+          Registrar Nueva cita
+        </Button>
 
-      <Button color="success" onClick={() => mostrarModalInsertar()}>
-        Registrar Nueva cita
-      </Button>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-
-      <Modal isOpen={modalInsertar}>
-        <Form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <div>
+            <img
+              className="imagen"
+              src="https://www.dalevidaatumascota.com/wp-content/uploads/2018/08/iStock-490056596-1024x683.jpg"
+            />
+          </div>
+          <div>
+            {citas.length > 0 && (
+              <Table>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nombre Mascota</th>
+                    <th>Propietario</th>
+                    <th>Fecha</th>
+                    <th>Hora</th>
+                    <th>Telefono</th>
+                    <th>Sintomas</th>
+                    <th>Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {citas.length > 0 &&
+                    citas?.map((dato) => {
+                      return (
+                        <tr key={dato.id}>
+                          <td>{dato.id}</td>
+                          <td>{dato.nombre_de_mascota}</td>
+                          <td>{dato.propietario}</td>
+                          <td>{dato.fecha}</td>
+                          <td>{dato.hora}</td>
+                          <td>{dato.telefono}</td>
+                          <td>{dato.sintomas}</td>
+                          <td>
+                            <Button
+                              color="primary"
+                              onClick={() => mostrarModalEditar(dato)}
+                            >
+                              Editar
+                            </Button>
+                            <Button color="danger">Cancelar</Button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            )}
+          </div>
+        </div>
+      </Container>
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar Registro</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label>Id</Label>
+            <Input
+              className="form-control"
+              readOnly
+              type="text"
+              value={input.id || ""}
+            />
+          </FormGroup>
           <FormGroup>
             <Label>Nombre de Mascota</Label>
             <Input
+              className="form-control"
               type="tex"
               name="nombre_de_mascota"
               placeholder="Ingrese nombre de la Mascota"
@@ -91,6 +178,7 @@ const Appointment_manager = () => {
           <FormGroup>
             <Label>Propietario</Label>
             <Input
+              className="form-control"
               type="tex"
               name="propietario"
               placeholder=" ingrese propietario"
@@ -101,6 +189,7 @@ const Appointment_manager = () => {
           <FormGroup>
             <Label>Telefono</Label>
             <Input
+              className="form-control"
               type="tex"
               name="telefono"
               placeholder="ingrese telefono"
@@ -111,6 +200,7 @@ const Appointment_manager = () => {
           <FormGroup>
             <Label>Fecha</Label>
             <Input
+              className="form-control"
               type="date"
               name="fecha"
               placeholder="ingrese fecha"
@@ -121,6 +211,7 @@ const Appointment_manager = () => {
           <FormGroup>
             <Label>Hora</Label>
             <Input
+              className="form-control"
               type="time"
               name="hora"
               placeholder="ingrese hora"
@@ -131,6 +222,7 @@ const Appointment_manager = () => {
           <FormGroup>
             <Label> Sintomas</Label>
             <textarea
+              className="form-control"
               type="tex"
               name="sintomas"
               placeholder="ingrese sintomas"
@@ -138,11 +230,97 @@ const Appointment_manager = () => {
               value={input.sintomas}
             />
           </FormGroup>
-          <Button
-            type="submit"
-            color="success"
-            onClick={() => cerrarModalInsertar()}
-          >
+        </ModalBody>
+
+        <ModalFooter>
+          <Button color="primary" onClick={() => editar(input)}>
+            Editar
+          </Button>
+          <Button color="danger" onClick={() => cerrarModalEditar()}>
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalInsertar}>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          <FormGroup>
+            <Label>Id</Label>
+            <Input
+              className="form-control"
+              readOnly
+              type="text"
+              value={citas?.length + 1}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Nombre de Mascota</Label>
+            <Input
+              className="form-control"
+              type="tex"
+              name="nombre_de_mascota"
+              placeholder="Ingrese nombre de la Mascota"
+              onChange={(e) => handleChange(e)}
+              value={input.nombre_de_mascota}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Propietario</Label>
+            <Input
+              className="form-control"
+              type="tex"
+              name="propietario"
+              placeholder=" ingrese propietario"
+              onChange={(e) => handleChange(e)}
+              value={input.propietario}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Telefono</Label>
+            <Input
+              className="form-control"
+              type="tex"
+              name="telefono"
+              placeholder="ingrese telefono"
+              onChange={(e) => handleChange(e)}
+              value={input.telefono}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Fecha</Label>
+            <Input
+              className="form-control"
+              type="date"
+              name="fecha"
+              placeholder="ingrese fecha"
+              onChange={(e) => handleChange(e)}
+              value={input.fecha}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Hora</Label>
+            <Input
+              className="form-control"
+              type="time"
+              name="hora"
+              placeholder="ingrese hora"
+              onChange={(e) => handleChange(e)}
+              value={input.hora}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label> Sintomas</Label>
+            <textarea
+              className="form-control"
+              type="tex"
+              name="sintomas"
+              placeholder="ingrese sintomas"
+              onChange={(e) => handleChange(e)}
+              value={input.sintomas}
+            />
+            <br />
+          </FormGroup>
+          <Button type="submit" color="success">
             Guardar Cita
           </Button>
         </Form>
